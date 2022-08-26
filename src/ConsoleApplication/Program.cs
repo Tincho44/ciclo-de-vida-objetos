02f955a4820488178759003e29ed3208ad006b45
Tincho44 <martinharbuet@gmail.com>
2022-08-26 00:23:15 -0300

    Subo las instancias de clase train t1, t2 y t3 al igual que el comparativo

--- src/ConsoleApplication/Program.cs
+++ src/ConsoleApplication/Program.cs
@@ -21,7 +21,13 @@ namespace ConsoleApplication
         {
             var train = new Train();
             train.StartEngines();
-            Console.WriteLine("Hello World!");
+            
+            Train t1 = new Train ("Last Train To London");
+            Train t2 = new Train ("Last Train To London");
+            Train t3 = new Train ("Runaway Train");
+            Console.WriteLine(t1 == t2); 
+            Console.WriteLine(t2 == t3);
+            // da False, mismos atributos pero eso no significa que sean el mismo objeto. Ambos hace referencia a otra parte de la memoria.
         }
     }
 }
